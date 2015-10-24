26fb3ab76213d7535015c25d2ec8554fa1cc8a93
jacobgroh <jacobgroh@email.arizona.edu>
2015-10-24 12:29:37 -0700

    new GUI BorderLayout

--- src/view/netPaintGUI.java
+++ src/view/netPaintGUI.java
@@ -7,13 +7,17 @@
  * JPanel, JRadioButtons, JColorChooser,...ect
  */
 package view;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.List;
 import java.awt.Shape;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.Line2D;
 import java.io.File;
 import java.io.IOException;
@@ -50,39 +54,63 @@ public class netPaintGUI extends JFrame {
 	private JColorChooser colors;
 	private JRadioButton  line, rectangel, oval, image;
 	private ButtonGroup   shapechoices;
-	private JPanel panel;
+	private JPanel panel, southPanel;
 	private DrawingPanel scrollPanel;
 	private JScrollPane canvus;
+	private JButton  colorChooser;
 	//Sets up the GUI to contain a JScrollPanel, ButtonGroup, and JColorChooser
 	private void layoutGUI() {
-		panel= new JPanel(null);
-		panel.setSize(1000, 630);
-		panel.setLocation(0, 0);
+		panel= new JPanel(new BorderLayout());
+		//panel.setSize(1000, 600);
+		//panel.setLocation(0, 0);
+		southPanel= new JPanel(null);
+		southPanel.setLocation(0, 0);
 		line= new JRadioButton("Line");
 		rectangel= new JRadioButton("Rectangel");
 		oval= new JRadioButton("Oval");
 		image= new JRadioButton("Image");
-		line.setSize(70, 40);
-		line.setLocation(345, 340);
-		rectangel.setSize(100, 40);
-		rectangel.setLocation(405, 340);
-		oval.setSize(70, 40);
-		oval.setLocation(505, 340);
-		image.setSize(100, 40);
-		image.setLocation(565, 340);
+		colorChooser= new JButton("Change Colors");
+//		line.setSize(70, 40);
+//		line.setLocation(345, 340);
+//		rectangel.setSize(100, 40);
+//		rectangel.setLocation(405, 340);
+//		oval.setSize(70, 40);
+//		oval.setLocation(505, 340);
+//		image.setSize(100, 40);
+//		image.setLocation(565, 340);
 		//add button listeners and actions...
+		line.setSize(70, 40);
+		line.setLocation(215, 20);
+		rectangel.setSize(100, 40);
+		rectangel.setLocation(275, 20);
+		oval.setSize(70, 40);
+		oval.setLocation(375, 20);
+		image.setSize(100, 40);
+		image.setLocation(435, 20);
+		colorChooser.setSize(140, 40);
+		colorChooser.setLocation(20, 20);
+		colorChooser.setVisible(true);
 
+		colorChooser.addActionListener(new colorButtonListener());
 		shapechoices= new ButtonGroup();
 		shapechoices.add(line);
 		shapechoices.add(rectangel);
 		shapechoices.add(oval);
 		shapechoices.add(image);
 
-		colors= new JColorChooser();
-		colors.setSize(1000, 320);
-		colors.setLocation(0, 380);
-		colors.setVisible(true);
-
+//		colors= new JColorChooser();
+//		colors.setSize(1000, 320);
+//		colors.setLocation(0, 70);
+//		colors.setVisible(true);
+		
+		//southPanel.add(colors);
+		southPanel.add(line);
+		southPanel.add(rectangel);
+		southPanel.add(oval);
+		southPanel.add(image);
+		southPanel.add(colorChooser);
+		southPanel.setPreferredSize(new Dimension(1000,100));
+		
 		scrollPanel = new DrawingPanel();
 		scrollPanel.setSize(1000, 380);
 		scrollPanel.setLocation(0, 0);
@@ -92,19 +120,32 @@ public class netPaintGUI extends JFrame {
 		
 		canvus= new JScrollPane(scrollPanel);
 		canvus.setSize(990, 340);
+
 		canvus.setLocation(0, 0);
 		canvus.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		canvus.setVisible(true);
 		
-		panel.add(colors);
-		panel.add(canvus);
-		this.add(line);
-		this.add(rectangel);
-		this.add(oval);
-		this.add(image);
+		//panel.add(colors);
+		//panel.add(canvus);
+
+		southPanel.setVisible(true);
+		panel.add(BorderLayout.SOUTH,southPanel);
+		panel.add(BorderLayout.CENTER,canvus);
 		this.add(panel);
 
 		this.setVisible(true);
+	}
+	private class colorButtonListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			Color color= scrollPanel.getBackground();
+			System.out.print(color.toString());
+
+			color= JColorChooser.showDialog(null, "Choose a Color", color );
+			System.out.print(color.toString());
+		}
+		
 	}
 	// Calls repaint on a list of picture objects
 	public void drawObjects(PaintObjectList pictures) {
@@ -135,8 +176,10 @@ public class netPaintGUI extends JFrame {
 					g2.fill((Shape)underObject);
 				}
 			}
-		}
+			//System.out.print("hello");
+		} 
 	}
+	
 	// Programs main that add five PaintObjects with thier dimensions and prints them
 	public static void main(String [] args){
 		PaintObjectList objectList= new PaintObjectList();
@@ -156,9 +199,9 @@ public class netPaintGUI extends JFrame {
 			e.printStackTrace();
 		}
 		
-		for (PaintObject pic : objectList) {
-			System.out.println(pic.getCurrentHeight());
-		}
+//		for (PaintObject pic : objectList) {
+//			System.out.println(pic.getCurrentHeight());
+//		}
 
 		netPaintGUI gui = new netPaintGUI();
 		
